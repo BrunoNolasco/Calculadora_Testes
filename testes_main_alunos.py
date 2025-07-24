8df3ff56e1be19dcfda9905114079cb829d0fa1d
BrunoNolasco <brunonolasco2763@gmail.com>
2025-07-24 12:19:57 +0100

    Update testes_main_alunos.py

--- testes_main_alunos.py
+++ testes_main_alunos.py
@@ -2,55 +2,57 @@ import unittest
 import math
 from main import calculadora, calculadora_v2, calculadora_v3, calculadora_v4
 
-
 class TestCalculadora(unittest.TestCase):
 
-    def teste_operacoes_basicas(self):
-        # Teste operações básicas de cada operador + - * / % ^
-        self.assertEqual(calculadora(2, 3, '+'), 5)
+    def executar_bateria_testes(self, func):
+        # Teste com operações básicas
+        self.assertEqual(func(2, 3, '+'), 5)
+        self.assertEqual(func(5, 3, '-'), 2)
+        self.assertEqual(func(2, 4, '*'), 8)
+        self.assertEqual(func(8, 2, '/'), 4)
+        self.assertEqual(func(9, 4, '%'), 1)
+        self.assertEqual(func(2, 3, '^'), 8)
 
-    def teste_v2_operacoes(self):
-        # Teste operações básicas de cada operador + - * / % ^
-        self.assertEqual(calculadora_v2(2, 3, '+'), 5)
+        # Teste com divisão por zero e módulo por zero
+        self.assertTrue(math.isnan(func(5, 0, '/')))
+        self.assertTrue(math.isnan(func(5, 0, '%')))
 
-    def teste_v3_operacoes(self):
-        # Teste operações básicas de cada operador + - * / % ^
-        self.assertEqual(calculadora_v3(2, 3, '+'), 5)
+        # Teste com operadores inválidos
+        self.assertTrue(math.isnan(func(2, 3, '$')))
+        self.assertTrue(math.isnan(func(2, 5, '#')))
+        self.assertTrue(math.isnan(func(0, 2, 'qwe')))
 
-    def teste_v4_operacoes(self):
-        # Teste operações básicas de cada operador + - * / % ^
-        self.assertEqual(calculadora_v4(2, 3, '+'), 5)
+        # Teste com números em ponto flutuante
+        self.assertAlmostEqual(func(2.5, 1.5, '+'), 4.0)
+        self.assertAlmostEqual(func(4.5, 1.5, '-'), 3.0)
+        self.assertAlmostEqual(func(5.5, 1.5, '*'), 8.25)
+        self.assertAlmostEqual(func(7.5, 1.5, '/'), 5.0)
+        self.assertAlmostEqual(func(7.5, 2.0, '%'), 1.5)
+        self.assertAlmostEqual(func(2.0, 3.0, '^'), 8.0)
 
-    def teste_operacoes_diversas(self):
-        # Teste divisão por zero operador para todas versões / %
-        self.assertTrue(math.isnan(calculadora(5, 0, '/')))
-        self.assertTrue(math.isnan(calculadora(5, 0, '%')))
+        # Teste com números negativos
+        self.assertEqual(func(-2, 3, '*'), -6)
+        self.assertEqual(func(-6, 3, '/'), -2.0)
+        self.assertEqual(func(-7, 3, '%'), 2.0)
+        self.assertEqual(func(-2, 3, '^'), -8)
+        self.assertEqual(func(0, 3, '^'), 0)
 
-        # Teste operador inválido - fazer três testes para todas as versões
-        self.assertTrue(math.isnan(calculadora(2, 3, '$')))
-        self.assertTrue(math.isnan(calculadora(2, 5, '#')))
-        self.assertTrue(math.isnan(calculadora(0, 2, 'qwe')))
+        # Chamar as operações das calculadoras (v1 - v4)
+    def test_calculadora_v1(self):
+        self.executar_bateria_testes(calculadora)
 
-        # Teste números de virgula flutuante - fazer três testes para todas as versões
-        self.assertAlmostEqual(calculadora(2.5, 1.5, '+'), 4.0)
-        self.assertAlmostEqual(calculadora(4.5, 1.5, '-'), 3.0)
-        self.assertAlmostEqual(calculadora(5.5, 1.5, '*'), 8.25)
+    def test_calculadora_v2(self):
+        self.executar_bateria_testes(calculadora_v2)
 
-        # Teste números negativos - fazer 3 testes para todas as versões
-        self.assertEqual(calculadora(-2, 3, '*'), -6)
+    def test_calculadora_v3(self):
+        self.executar_bateria_testes(calculadora_v3)
 
-        # Teste números negativos com divisão e módulo, testar para todas as versões
-        self.assertTrue(calculadora(-6, 3, '/'), -2.0)
-        self.assertTrue(calculadora(-7, 3, '%'), 2.0)
-
-        # Teste números negativos com exponenciação, testar para todas as versões
-        self.assertEqual(calculadora(-2, 3, '^'), -8)
-
-        # Teste números negativos com exponenciação de zero, testar para todas as versões
-        self.assertEqual(calculadora(0, 3, '^'), 0)
+    def test_calculadora_v4(self):
+        self.executar_bateria_testes(calculadora_v4)
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
 # para correr os testes: python -m unittest -v testes_main_alunos.py
